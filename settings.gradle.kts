527eb1824a7cac5929b53c4dc4c7552ba3dba297
Delphox <smugleaf@live.com>
2024-10-31 18:19:47 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,19 @@
-include(":CopyInsteadOfShareImages")
-project(":CopyInsteadOfShareImages").projectDir = File("./CopyInsteadOfShareImages")
+//include(":CopyInsteadOfShareImages")
+//project(":CopyInsteadOfShareImages").projectDir = File("./CopyInsteadOfShareImages")
 
-include(":MediaPickerPatcher")
-project(":MediaPickerPatcher").projectDir = File("./MediaPickerPatcher")
+//include(":MediaPickerPatcher")
+//project(":MediaPickerPatcher").projectDir = File("./MediaPickerPatcher")
 
-include(":AttachmentKeyboardFix")
-project(":AttachmentKeyboardFix").projectDir = File("./AttachmentKeyboardFix")
+//include(":AttachmentKeyboardFix")
+//project(":AttachmentKeyboardFix").projectDir = File("./AttachmentKeyboardFix")
 
 include(":TwitterRebuild")
 project(":TwitterRebuild").projectDir = File("./TwitterRebuild")
 
-include(":FakeStickers")
-project(":FakeStickers").projectDir = File("./FakeStickers")
+//include(":FakeStickers")
+//project(":FakeStickers").projectDir = File("./FakeStickers")
 
-include(":NS4NQN")
-project(":NS4NQN").projectDir = File("./NitroSpoofForNQN")
+//include(":NS4NQN")
+//project(":NS4NQN").projectDir = File("./NitroSpoofForNQN")
 
 rootProject.name = "AliucordPlugins"
