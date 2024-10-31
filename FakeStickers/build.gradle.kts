3e27b20ff55685dbe5ab2bbec20cdbb20246e4e9
Delphox <smugleaf@live.com>
2024-10-31 13:08:42 -0300

    bump version

--- FakeStickers/build.gradle.kts
+++ FakeStickers/build.gradle.kts
@@ -1,10 +1,12 @@
-version = "1.1.2"
+version = "1.1.3"
 description = "Posts a sticker as an image if the sticker is unavailable normally (Usually when you don't have Nitro). Lottie stickers are unsupported."
 
 aliucord {
     changelogMedia.set("https://cdn.discordapp.com/stickers/883809297216192573.png")
     changelog.set(
         """
+            # 1.1.3
+            * Support GIF stickers
             # 1.1.1
             * Support Discord 105.12
             # 1.1.0
