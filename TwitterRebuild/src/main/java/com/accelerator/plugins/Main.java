188239da16708d8f889619164c00d7022a3864be
Delphox <smugleaf@live.com>
2024-10-31 17:05:01 -0300

    Update Main.java

--- TwitterRebuild/src/main/java/com/accelerator/plugins/Main.java
+++ TwitterRebuild/src/main/java/com/accelerator/plugins/Main.java
@@ -105,7 +105,7 @@ public class Main extends Plugin {
 
                         List<EmbedEntry> embedAndAttachmentList = (List)callFrame.getResult();
                         List<MessageEmbed> embeds = message.getEmbeds();
-
+			new Logger("TwitterRebuild").debug(embeds);
                         if (embedAndAttachmentList.size() < 1 || embeds.size() < 1 || embedAndAttachmentList.size() == embeds.size()) return;
                         long guildID = embedAndAttachmentList.get(0).getGuildId();
 
