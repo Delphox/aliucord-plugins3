89fd4a33b0aefc7fa8a5b9b73d7dcf39079a247e
Delphox <smugleaf@live.com>
2026-03-24 17:48:47 -0300

    forgot to revert the previous thing lmao

--- MediaPickerPatcher/src/main/java/com/accelerator/plugins/MediaPickerPatcher.java
+++ MediaPickerPatcher/src/main/java/com/accelerator/plugins/MediaPickerPatcher.java
@@ -101,12 +101,11 @@ public class MediaPickerPatcher extends Plugin {
 				pickerButton.setOnClickListener( new View.OnClickListener() {
 					@Override
 					public void onClick (View v) {
-						if (settings.getBool("MMP_AllowAllFiles", false)) {
-							intent = new Intent(Intent.ACTION_GET_CONTENT);
+						Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+						if (settings.getBool("MMP_AllowAllFiles", false))
 							intent.setType("*/*");
-						} else
+						else
 						{
-							intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
 							intent.setType("image/* video/*");
 							intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[] {"image/*", "video/*"});
 						}
